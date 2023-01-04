71b5d55c8b40df01976b679aaf9818602a2f47de
Carl Oscar Aaro <hello@carloscar.com>
2023-01-04 16:21:20 +0100

    22 - Don't install .gitconfig in codespaces

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
 [ -e .profile ] && ln -f -s ${PWD}/.profile ~/.profile && printf "\n\n. ~/.profile\n" >> ~/.bashrc
-[ -e .gitconfig ] && ln -f -s ${PWD}/.gitconfig ~/.gitconfig
 [ -e .vimrc ] && ln -f -s ${PWD}/.vimrc ~/.vimrc
